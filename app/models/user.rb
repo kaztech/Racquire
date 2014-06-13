7347f89c322b2db5072b9cc1c854169d0025c1f0
kaztech <ameen@ktg.biz>
2014-06-13 01:15:41 -0400

    added facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,18 +11,37 @@ class User < ActiveRecord::Base
       return user
     else
       registered_user = User.where(:email => access_token.info.email).first
+       if registered_user
+        return registered_user
+       else
+        user = User.create(name: data["name"],
+                          provider:access_token.provider,
+                          email: data["email"],
+                          uid: access_token.uid ,
+                          password: Devise.friendly_token[0,20],
+        )
+	     end
+		end
+
+    end
+
+def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
       if registered_user
         return registered_user
       else
-        user = User.create(name: data["name"],
-          provider:access_token.provider,
-          email: data["email"],
-          uid: access_token.uid ,
-          password: Devise.friendly_token[0,20],
-        )
-		      end
-		   end
-		end
-
+        user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20],
+                          )
+      end 
+    end
+  end
 
 end
