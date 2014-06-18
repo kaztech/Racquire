b3501e1ae8a87120a4d4e45abeff354a1d81a119
kaztech <ameen@ktg.biz>
2014-06-18 16:48:36 -0400

    testing new signup and sign in with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+def after_sign_in_path_for(resource_or_scope)
+  session.fetch 'user_return_to', recruiter_dash_path
+end
+
 end
