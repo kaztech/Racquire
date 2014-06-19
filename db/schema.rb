99e14c7bbaf1685bcc32b14c4d732a3d355e863f
kaztech <ameen@ktg.biz>
2014-06-19 11:28:09 -0400

    added fields to new member login

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20140612182023) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
+    t.string   "current_employer"
+    t.string   "current_job_title"
+    t.string   "skills"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
