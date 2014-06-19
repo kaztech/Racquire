44fe9cc18635a2becbf8900c06ec0d6d7dacdc86
kaztech <ameen@ktg.biz>
2014-06-19 11:36:18 -0400

    changed name to full name

--- db/migrate/20140612182023_add_provider_to_users.rb
+++ db/migrate/20140612182023_add_provider_to_users.rb
@@ -2,7 +2,7 @@ class AddProviderToUsers < ActiveRecord::Migration
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
-    add_column :users, :name, :string
+    add_column :users, :full_name, :string
     add_column :users, :current_employer, :string
     add_column :users, :current_job_title, :string
     add_column :users, :skills, :string
