80a2e6aa22d169dff78cbf50c5228e6430286e98
kaztech <ameen@ktg.biz>
2014-06-19 11:10:35 -0400

    added in addl fields for user model

--- db/migrate/20140611181314_devise_create_users.rb
+++ db/migrate/20140611181314_devise_create_users.rb
@@ -2,9 +2,12 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :current_employer,   null: false, default: ""
+      t.string :current_job_title,  null: false, default: ""
+      t.string :skills,             null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/migrate/20140612182023_add_provider_to_users.rb
+++ db/migrate/20140612182023_add_provider_to_users.rb
@@ -2,5 +2,9 @@ class AddProviderToUsers < ActiveRecord::Migration
   def change
     add_column :users, :provider, :string
     add_column :users, :uid, :string
+    add_column :users, :name, :string
+    add_column :users, :current_employer, :string
+    add_column :users, :current_job_title, :string
+    add_column :users, :skills, :string
   end
 end
