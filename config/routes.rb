cfc481692e4a2d60738781c393fa23a94e6f5a0f
kaztech <ameen@ktg.biz>
2014-06-19 20:38:41 -0400

    still trying to get 2 user models in devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :employers, :controllers
+  devise_for :employers
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  
   devise_for :models
   
   root "pages#home"
