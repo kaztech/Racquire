80720bcb1aa2a86cc6301bcf785c826236ae4506
Kory Kraft <kraft.kory@gmail.com>
2014-06-29 19:23:28 -0700

    allowing more fields for devise - adding in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-def after_sign_in_path_for(resource_or_scope)
-  session.fetch 'user_return_to', recruiter_dash_path
-end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource_or_scope)
+    session.fetch 'user_return_to', recruiter_dash_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+  end
 
 end
